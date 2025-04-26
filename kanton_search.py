6942d94342311bafcc6be71b888df6feb42bbb25
Paul Frischknecht <paulfrischknecht@paulfrischknecht.ch>
2025-04-26 13:43:38 +0200

    attempt to fix existing property listing logi; unfortunately comparis has datadome api protection now, returns 403 most of the time...

--- kanton_search.py
+++ kanton_search.py
@@ -3,9 +3,14 @@ the distances to our workplaces.
 
 SEARCH_DEFAULTS contains the hard coded search requirements.
 
-Requires:
-    GOOGLE_API_KEY to be set to a Google API key.
+Requires env variables:
+    GOOGLE_API_KEY to be set to a Google (Maps) API key, e.g. obtained from https://console.cloud.google.com/google/maps-apis/onboard;flow=gmp-api-key-flow (APIs and Services > Google Maps Platform)
     DESTINATIONS to be set to the Google Maps strings to compare distances to.
+
+Usage example:
+
+    PIP_BREAK_SYSTEM_PACKAGES=1 pip install -r requirements.txt
+    DESTINATIONS='["Bern"]' GOOGLE_API_KEY='AIzaSyAoMlWx*****' python kanton_search.py
 """
 import re
 import json
@@ -13,6 +18,7 @@ import itertools
 import datetime
 import logging
 import argparse
+import os
 from functools import reduce
 
 import yaml
@@ -30,7 +36,7 @@ SEARCH_DEFAULTS = {
     'RoomsFrom': '2', 'RoomsTo': '4',
     'FloorSearchType': '0',
     'LivingSpaceFrom': None, 'LivingSpaceTo': None,
-    'PriceFrom': 700, 'PriceTo': '2000',
+    'PriceFrom': 600, 'PriceTo': '2000',
     'ComparisPointsMin': '4',
     'AdAgeMax': '1',
     'AdAgeInHoursMax': None,
@@ -51,13 +57,35 @@ SEARCH_DEFAULTS = {
 }
 
 # Google API
-GOOGLE_API_KEY = ''
 ARRIVAL_UTC = datetime.time(hour=8)  # 9am Zurich time
+#GOOGLE_API_KEY = ''
+# init GOOGLE_API_KEY from environment variable:
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 
 # Other constants
 DEBUG_MODE = False
 MAX_TRAVEL_TIME = 40 * 60  # 40 minutes
-DESTINATIONS = []
+#DESTINATIONS = []
+# parse DESTINATIONS from environment variable as JSON
+DESTINATIONS = json.loads(os.environ.get('DESTINATIONS', '[]'))
+
+# Verbose logging for requests module (debugging)
+# Taken from https://stackoverflow.com/questions/10588644/how-can-i-see-the-entire-http-request-thats-being-sent-by-my-python-application
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 
 # Taken from http://stackoverflow.com/a/6558571
@@ -73,18 +101,20 @@ def next_weekday(date_time, weekday):
 
 def get_listings(page: int):
     """Get the listings of the page numbers."""
-    logging.info("Fetching listings for page: %d.", page)
-    payload = {
-        'sort': 3,
-        'page': page
-    }
+    logging.info("get_listings fetching listings for page: %d.", page)
     params = json.dumps(SEARCH_DEFAULTS, separators=(',', ':'))
-    url = COMPARIS_URL + '?requestobject=' + params
-    response = requests.get(url, params=payload)
+    #url = COMPARIS_URL + '?requestobject=' + params
+    # use a proper library to encode the params as a query string (encodeURIComponent style):
+    url = COMPARIS_URL + '?requestobject=' + requests.utils.quote(params)
+
+    logging.info("get_listings URL: %s", url)
+    response = requests.get(url)
     if response.status_code != 200:
-        logging.info("Request failed with HTTP code: %d", response.status_code)
-        raise ValueError('Page is out of range.')
+        logging.info("get_listings Request failed with HTTP code: %d", response.status_code)
+        raise ValueError('get_listings Request failed with HTTP code: %d' % response.status_code)
     else:
+        logging.info("get_listings Request succeeded with HTTP code: %d, "
+                     "length: %d, content [...]: %s", response.status_code, len(response.content), response.content[:100])
         results = _parse_content(response.content)
         logging.info("Fetched %d listings.", len(results))
         return results
@@ -171,17 +201,17 @@ def _distance_error(place) -> bool:
     return any(distance is None for distance in place['distances'].values())
 
 
-# def _zurich_special(place) -> bool:
-#     """Returns true if Sana's work place is within 55minutes and JP's within
-#     35minutes.
-#     """
-#     SANA_TRAVEL_TIME = 55 * 60
-#     JP_TRAVEL_TIME = 35 * 60
-#     # All distance have a value and Zurich area code 80[0-9][0-9]
-#     if _distance_error(place) or not re.search(r'80\d\d', place['address']):
-#         return False
-#     return (place['distances'][SANA_OFFICE]['value'] <= SANA_TRAVEL_TIME and
-#             place['distances'][JP_OFFICE]['value'] <= JP_TRAVEL_TIME)
+def _zurich_special(place) -> bool:
+    """Returns true if Sana's work place is within 55minutes and JP's within
+    35minutes.
+    """
+    SANA_TRAVEL_TIME = 55 * 60
+    JP_TRAVEL_TIME = 35 * 60
+    # All distance have a value and Zurich area code 80[0-9][0-9]
+    if _distance_error(place) or not re.search(r'80\d\d', place['address']):
+        return False
+    return (place['distances'][SANA_OFFICE]['value'] <= SANA_TRAVEL_TIME and
+            place['distances'][JP_OFFICE]['value'] <= JP_TRAVEL_TIME)
 
 
 def main():
@@ -198,7 +228,10 @@ def main():
     try:
         for page_num in page_numbers:
             places += get_listings(page_num)
-    except ValueError:
+            # HACK throttle (wait for 5 seconds), to avoid getting blocked (403) by Comparis' "datadome" protection
+            time.sleep(5)
+    except Exception as e:
+        logging.warning("No more pages to fetch or other error: %s", e)
         pass
 
     # Add distances in batches of 10
